ba25d8e3e20b74b7fc3e34b678052b740fae1007
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-10-28 00:48:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "USER";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome,  ${currentUser.slice(0, 1)}!`;
